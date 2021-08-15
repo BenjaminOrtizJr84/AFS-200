875515e935555f09d7197bc01e4c4dd9301563aa
BenjaminOrtizJr84 <85262543+BenjaminOrtizJr84@users.noreply.github.com>
2021-08-15 12:57:47 -0400

    Week 2 Assignment

---
+++ week2/wk2_assignment/odd_even.py
@@ -0,0 +1,8 @@
+# Odd or Even
+
+userNumber = int(input("Please Enter A Number: "))
+
+if (userNumber % 2) == 0:
+    print("{0} is Even".format(userNumber))
+else:
+    print("{0} is Odd".format(userNumber))
