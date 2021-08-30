1cbdf1e389900409604c23c27933a7003e06e288
BenjaminOrtizJr84 <85262543+BenjaminOrtizJr84@users.noreply.github.com>
2021-08-30 12:55:45 -0400

    Update guessing_game.py

--- week4/w4_assignment/guessing_game.py
+++ week4/w4_assignment/guessing_game.py
@@ -1,16 +1,16 @@
 import random
 
 n = random.randint(1, 9)
-guess = int(input("Enter an integer from 1 to 9: "))
+guess = int(input('Enter a number from 1 to 9: '))
 while n != "guess":
     print
     if guess < n:
-        print("guess is low")
-        guess = int(input("Enter an integer from 1 to 9: "))
+        print("Guess is low")
+        guess = int(input('Enter an number from 1 to 9: '))
     elif guess > n:
-        print("guess is high")
-        guess = int(input("Enter an integer from 1 to 9: "))
+        print("Guess is high")
+        guess = int(input('Enter an number from 1 to 9: '))
     else:
-        print("you guessed it!")
+        print('You guessed it! Game Over.')
         break
-    print
+    
